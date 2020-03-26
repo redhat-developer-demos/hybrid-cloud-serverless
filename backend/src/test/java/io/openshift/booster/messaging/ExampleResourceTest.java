2d21e21ba2145aebbdd7ffd805575a53635f791a
sebastienblanc <scm.blanc@gmail.com>
2020-03-26 14:23:04 +0100

    Add Skupper doc, fix tests, fix frontend service name

--- backend/src/test/java/io/openshift/booster/messaging/ExampleResourceTest.java
+++ backend/src/test/java/io/openshift/booster/messaging/ExampleResourceTest.java
@@ -4,18 +4,20 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 public class ExampleResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testWorkerEndpoint() {
         given()
-          .when().get("/hello")
+          .body("{\"requestId\" : 1337,\"request\" :{\"text\":\"foo\",\"uppercase\" : false,\"reverse\" : false }}")
+          .contentType("application/json")
+          .when().post("/api/process")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(containsString("Aloha foo"));
     }
 
 }
--- frontend/src/test/java/io/openshift/booster/messaging/ExampleResourceTest.java
+++ frontend/src/test/java/io/openshift/booster/messaging/ExampleResourceTest.java
@@ -12,10 +12,9 @@ public class ExampleResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/api/data")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+             .statusCode(200);
     }
 
 }
