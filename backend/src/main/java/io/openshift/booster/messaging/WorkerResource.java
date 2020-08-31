0ca24f9bf554971e4de12ff1e266da61236392a5
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-08-31 10:33:53 +0530

    update res

--- backend/src/main/java/io/openshift/booster/messaging/WorkerResource.java
+++ backend/src/main/java/io/openshift/booster/messaging/WorkerResource.java
@@ -3,11 +3,11 @@ package io.openshift.booster.messaging;
 import java.util.UUID;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/api")
@@ -15,31 +15,38 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @Consumes(MediaType.APPLICATION_JSON)
 public class WorkerResource {
 
-    private static final String ID = "worker-quarkus-" + UUID.randomUUID()
-                                        .toString().substring(0, 4);
+  private static final String ID = "worker-quarkus-" + UUID.randomUUID().toString().substring(0, 4);
 
-    @ConfigProperty(name = "worker.cloud.id", defaultValue = "unknown")
-    String cloudId;
+  @ConfigProperty(name = "worker.cloud.id", defaultValue = "unknown")
+  String cloudId;
 
-    @POST
-    @Path("/process")
-    public Response process(Message message) {
-        System.out.println(message);
-        Response response = new Response(message.getRequestId(), ID, cloudId, process(message.getRequest()));
-        return response;
+
+  @GET
+  @Path("/cloud")
+  public String cloud() {
+    return cloudId;
+  }
+
+  @POST
+  @Path("/process")
+  public Response process(Message message) {
+    System.out.println(message);
+    Response response =
+        new Response(message.getRequestId(), ID, cloudId, process(message.getRequest()));
+    return response;
+  }
+
+  private String process(Request request) {
+
+    String text = request.getText();
+
+    if (request.isUppercase()) {
+      text = text.toUpperCase();
     }
 
-    private String process(Request request) {
-    
-        String text = request.getText();
-
-        if (request.isUppercase()) {
-            text = text.toUpperCase();
-        }
-    
-        if (request.isReverse()) {
-            text = new StringBuilder(text).reverse().toString();
-        }
-        return "Aloha " + text;
+    if (request.isReverse()) {
+      text = new StringBuilder(text).reverse().toString();
     }
+    return "Aloha " + text;
+  }
 }
